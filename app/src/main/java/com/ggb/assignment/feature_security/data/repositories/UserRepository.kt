fbb5852c577c20f43aa1aa8f80d4c131aa0f95cc
Gandhi Balingkit <BalingkitG@steag-spi.local>
2022-09-06 22:27:01 +0800

    change hardcoded username and password

--- app/src/main/java/com/ggb/assignment/feature_security/data/repositories/UserRepository.kt
+++ app/src/main/java/com/ggb/assignment/feature_security/data/repositories/UserRepository.kt
@@ -4,7 +4,7 @@ import com.ggb.assignment.feature_security.domain.contracts.IUserRepository
 
 class UserRepository : IUserRepository{
     override suspend fun login(email: String, password: String): Boolean {
-        if(email == "magneds" && password == "magneds")
+        if(email == "ggb" && password == "ggb")
             return true
         return false
     }
